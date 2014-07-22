bea95ecfc9111f0f05b3dbf9d178bdc32eb49f15
siamang <hkoh59@gmail.com>
2014-07-22 16:50:29 -0500

    Added autosave feature

--- popup.js
+++ popup.js
@@ -1,7 +1,39 @@
 // popup.js
 
-var a = document.getElementsByClassName("close-box")[0];
 
-a.addEventListener("click", function() {
+var timeout;
+
+// close popup.html
+var closeNote = document.getElementsByClassName("close-box")[0];
+closeNote.addEventListener("click", function() {
     window.close();
 })
+
+// add new notes
+var newNote = document.getElementsByTagName("textarea")[0];
+newNote.addEventListener("input", addNote);
+
+function addNote(e) {
+    var text = this.value;
+    console.log(text);
+
+    // autosave feature
+    active();
+    inactive();
+}
+
+
+
+function inactive() {
+    // Saves text shortly after a keyboard event.
+    // Function active() inactivates "timeout"
+    timeout = window.setTimeout(function() {
+        console.log("saved");
+    }, 350);
+}
+
+function active() {
+    // if keyboard event is dectected, 
+    // removes timeout and does not save anything from the textbox
+    window.clearTimeout(timeout);
+}
