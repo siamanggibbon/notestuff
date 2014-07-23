6347a833514b038be6ebdf41ddfc326a53ed87c3
siamang <hkoh59@gmail.com>
2014-07-23 01:30:45 -0500

    refactored and added textarea outlie on save

--- popup.js
+++ popup.js
@@ -1,44 +1,65 @@
 // popup.js
 
-var timeout;
+(function() {
 
-// add new notes
-var newNote = document.getElementsByTagName("textarea")[0];
-newNote.addEventListener("input", addNote, false);
+    var timeout,
+        status,
+        newNode,
+        saved,
+        statusRemove;
 
-// get saved notes into textbox
-var saved = localStorage.getItem("note");
-if (saved) {
+    newNote = document.getElementsByTagName("textarea")[0];
+
+    if (localStorageAvail() === false) {
+        newNote.value = "There is a problem with localStorage.  Notes will not be saved.";
+        newNote.disabled = true;
+        return;
+    }
+
+    // add new notes
+    newNote.addEventListener("input", addNote, false);
+
+    // get saved notes into textbox
+    saved = localStorage.getItem("note");
+    console.log(saved);
     newNote.value = saved;
-}
 
-function addNote(e) {
-    var text = this.value;
-    console.log(text);
+    function addNote(e) {
+        // autosave feature
+        active();
+        inactive(this.value);
+    }
 
-    // autosave feature
-    active();
-    inactive(text);
-}
+    function inactive(text) {
+        // Saves text shortly after a keyboard event.
+        // active() inactivates "timeout"
+        timeout = window.setTimeout(function() {
+            localStorage.setItem("note", text);
+            newNode = document.getElementsByTagName("textarea")[0];
+            newNode.className = "saved";
+            status = document.getElementsByTagName("p")[0];
+            status.className = "status fade";
+            statusRemove = window.setTimeout(function() {
+                newNode.className = "";
+                status.className = "status";
+            }, 2200);
+        }, 200);
+    }
 
-function inactive(text) {
-    // Saves text shortly after a keyboard event.
-    // Function active() inactivates "timeout"
-    timeout = window.setTimeout(function() {
-        localStorage.setItem("note", text);
-        var status = document.getElementsByTagName("p")[0];
-        status.className = "status fade";
-        var statusRemove = window.setTimeout(function() {
-            status.className = "status";
-        }, 1000);
+    function active() {
+        status = document.getElementsByTagName("p")[0];
+        status.className = "status";
+        // if keyboard event is dectected, 
+        // removes timeout and does not save anything from the textbox
+        window.clearTimeout(timeout);
+    }
 
-    }, 350);
-}
+    function localStorageAvail() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch (e) {
+            return false;
+        }
+    }
 
-function active() {
-    var status = document.getElementsByTagName("p")[0];
-    status.className = "status";
-    // if keyboard event is dectected, 
-    // removes timeout and does not save anything from the textbox
-    window.clearTimeout(timeout);
-}
+})();
