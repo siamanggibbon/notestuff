1bc570ae961874240dadeed466f10f04497f7b9a
siamang <hkoh59@gmail.com>
2014-07-22 21:17:52 -0500

    removed saved and close button

--- popup.js
+++ popup.js
@@ -1,17 +1,16 @@
 // popup.js
 
-
 var timeout;
-
-// close popup.html
-var closeNote = document.getElementsByClassName("close-box")[0];
-closeNote.addEventListener("click", function() {
-    window.close();
-})
 
 // add new notes
 var newNote = document.getElementsByTagName("textarea")[0];
-newNote.addEventListener("input", addNote);
+newNote.addEventListener("input", addNote, false);
+
+// get saved notes into textbox
+var saved = localStorage.getItem("note");
+if (saved) {
+    newNote.value = saved;
+}
 
 function addNote(e) {
     var text = this.value;
@@ -19,20 +18,26 @@ function addNote(e) {
 
     // autosave feature
     active();
-    inactive();
+    inactive(text);
 }
 
-
-
-function inactive() {
+function inactive(text) {
     // Saves text shortly after a keyboard event.
     // Function active() inactivates "timeout"
     timeout = window.setTimeout(function() {
-        console.log("saved");
+        localStorage.setItem("note", text);
+        var status = document.getElementsByTagName("p")[0];
+        status.className = "status fade";
+        var statusRemove = window.setTimeout(function() {
+            status.className = "status";
+        }, 1000);
+
     }, 350);
 }
 
 function active() {
+    var status = document.getElementsByTagName("p")[0];
+    status.className = "status";
     // if keyboard event is dectected, 
     // removes timeout and does not save anything from the textbox
     window.clearTimeout(timeout);
